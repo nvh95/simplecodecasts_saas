af49575dbf8da40778f507072e8042e45f145139
Nguyen Viet Hung <nguyenviethunghl00@gmail.com>
2015-09-05 17:27:59 +0000

    Fixed typo in Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  before_filter :configure_permitted_paremeter, if: :devise_controller?
+  before_filter :configure_permitted_parameter, if: :devise_controller?
   
   protected
-  def configure_permitted_paremeter
-    devise_paramater_sanitizer.for(:signup) { |u| u.permit(:name, :stripe_card_token, :email, :password, :passwordconfirmation)}
+  def configure_permitted_parameter
+    devise_parameter_sanitizer.for(:signup) { |u| u.permit(:name, :stripe_card_token, :email, :password, :passwordconfirmation)}
   end
 end
